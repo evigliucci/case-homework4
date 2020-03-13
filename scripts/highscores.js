2d36e1ddfb971d80f93456d3680ce7b9b60d8d68
Erika M Vigliucci <a094946@c02xv1fljg5m.prog1.com>
2020-03-13 18:14:11 -0400

    start over with starter code

---
+++ scripts/highscores.js
@@ -0,0 +1,13 @@
+function printHighscores() {
+  // either get scores from localstorage or set to empty array
+
+
+  // sort highscores by score property in descending order
+
+}
+
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+
